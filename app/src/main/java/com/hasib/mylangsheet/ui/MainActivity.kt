71756d2554feec3999be8d90ce65c92ab82dcfe2
devhsb <hasibpanjshery2@gmail.com>
2024-04-11 21:29:15 +0100

    practice section improved

--- app/src/main/java/com/hasib/mylangsheet/ui/MainActivity.kt
+++ app/src/main/java/com/hasib/mylangsheet/ui/MainActivity.kt
@@ -7,12 +7,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.hasib.mylangsheet.ui.screens.navigation.Navigation
+import com.hasib.mylangsheet.ui.screens.words.wordmain.WordViewModel
 import com.hasib.mylangsheet.ui.screens.words.wordmain.WordsScreen
 import com.hasib.mylangsheet.ui.theme.MyLangsheetTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private lateinit var navController: NavHostController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,7 +28,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    WordsScreen()
+                    navController = rememberNavController()
+                    val wordViewModel : WordViewModel = viewModel()
+                    Navigation(navController, wordViewModel = wordViewModel)
                 }
             }
         }
