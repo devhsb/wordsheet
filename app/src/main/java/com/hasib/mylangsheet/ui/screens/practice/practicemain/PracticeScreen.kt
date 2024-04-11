da20687c00755071389757352707ab5c157efaf1
devhsb <hasibpanjshery2@gmail.com>
2024-04-11 21:29:43 +0100

    practice section improved

---
+++ app/src/main/java/com/hasib/mylangsheet/ui/screens/practice/practicemain/PracticeScreen.kt
@@ -0,0 +1,17 @@
+package com.hasib.mylangsheet.ui.screens.practice.practicemain
+
+import androidx.compose.runtime.Composable
+import com.hasib.mylangsheet.ui.screens.words.wordmain.WordViewModel
+
+@Composable
+fun PracticeScreen(
+    wordViewModel: WordViewModel,
+    onWordItemClicked: () -> Unit,
+    onPracticeItemClicked: () -> Unit,
+) {
+    PracticeContent(
+        wordViewModel = wordViewModel,
+        onWordItemClicked = onWordItemClicked,
+        onPracticeItemClicked = onPracticeItemClicked
+    )
+}
