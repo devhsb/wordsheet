58ecea45bba17df8fb64fd8485d11d7b7d236cbc
devhsb <hasibpanjshery2@gmail.com>
2024-04-11 21:30:06 +0100

    practice section improved

--- app/src/main/java/com/hasib/mylangsheet/ui/shared_components/TopAppbar.kt
+++ app/src/main/java/com/hasib/mylangsheet/ui/shared_components/TopAppbar.kt
@@ -1,38 +1,132 @@
 package com.hasib.mylangsheet.ui.shared_components
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.slideInVertically
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.hasib.mylangsheet.ui.screens.words.wordmain.WordViewModel
 import com.hasib.mylangsheet.ui.theme.MyLangsheetTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CenterAlignedTopAppbar(
-    title: String
+    title: String,
+    onWordItemClicked: () -> Unit,
+    onPracticeItemClicked: () -> Unit,
+    wordViewModel: WordViewModel
 ) {
-    CenterAlignedTopAppBar(title = {
-        Text(
-            text = title,
-            style = MaterialTheme.typography.headlineMedium,
-            fontWeight = FontWeight.Bold
-        )
-    })
+    Column {
+        var expanded by wordViewModel.topbarDropDownState
+
+        CenterAlignedTopAppBar(title = {
+            Row(
+                modifier = Modifier
+                    .clickable { expanded = !expanded },
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.headlineMedium,
+                    fontWeight = FontWeight.Bold
+                )
+
+                Spacer(modifier = Modifier.width(5.dp))
+
+                Icon(
+                    imageVector = Icons.Filled.KeyboardArrowDown,
+                    contentDescription = null
+                )
+            }
+        })
+
+
+        DropdownMenu(
+            modifier = Modifier
+                .fillMaxWidth(),
+            expanded = expanded,
+            onDismissRequest = {expanded = !expanded}) {
+
+            DropdownMenuItem(
+                text = "Words",
+                onItemClicked = onWordItemClicked
+            )
+            DropdownMenuItem(
+                text = "Practice",
+                onItemClicked = onPracticeItemClicked
+            )
+        }
+
+    }
+}
+
+@Composable
+fun DropdownMenuItem(
+    text: String,
+    onItemClicked: () -> Unit
+) {
+    DropdownMenuItem(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(5.dp)
+            .border(
+                width = 1.dp,
+                color = MaterialTheme.colorScheme.onSecondary,
+                shape = CutCornerShape(5.dp)
+            ),
+        text = {
+            Text(
+                text = text,
+                textAlign = TextAlign.Center
+            )
+        },
+        onClick = onItemClicked
+    )
 }
 
 @Preview
 @Composable
-fun CenterAlignedTopAppbarPreview(){
-    CenterAlignedTopAppbar(title = "Words")
+fun CenterAlignedTopAppbarPreview() {
+    CenterAlignedTopAppbar(title = "Words", {}, {}, viewModel())
 }
 
 @Preview
 @Composable
-fun CenterAlignedTopAppbarPreviewDark(){
+fun CenterAlignedTopAppbarPreviewDark() {
     MyLangsheetTheme(useDarkTheme = true) {
-        CenterAlignedTopAppbar(title = "Words")
+        CenterAlignedTopAppbar(title = "Words", {}, {}, viewModel())
     }
 }
