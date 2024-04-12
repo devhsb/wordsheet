f4f6444d2ff6f1e96a545277ebff9984f84500b1
devhsb <hasibpanjshery2@gmail.com>
2024-04-12 16:53:52 +0100

    practice section improved

--- app/src/main/java/com/hasib/mylangsheet/ui/screens/practice/practicemain/PracticeContent.kt
+++ app/src/main/java/com/hasib/mylangsheet/ui/screens/practice/practicemain/PracticeContent.kt
@@ -1,7 +1,6 @@
 package com.hasib.mylangsheet.ui.screens.practice.practicemain
 
-import android.widget.Space
-import androidx.compose.foundation.background
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,21 +10,23 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.hasib.mylangsheet.data.model.Word
 import com.hasib.mylangsheet.ui.screens.words.wordmain.WordCard
 import com.hasib.mylangsheet.ui.screens.words.wordmain.WordViewModel
 import com.hasib.mylangsheet.ui.shared_components.CenterAlignedTopAppbar
+import kotlin.random.Random
 
 @Composable
 fun PracticeContent(
@@ -35,6 +36,14 @@ fun PracticeContent(
 ) {
 
     val words by wordViewModel.wordList.collectAsState()
+
+    val passedWord: MutableList<Word> = words.toMutableList()
+
+    var randomWord by remember {
+        mutableStateOf(passedWord.random())
+    }
+
+
 
     Scaffold(
         topBar = {
@@ -48,10 +57,22 @@ fun PracticeContent(
 
     ) {
         Box(modifier = Modifier.padding(it)) {
+
             PracticeBody(
-                word = words[1],
-                openSimpleDialog = {}
+                word = randomWord,
+
+                openSimpleDialog = {
+
+                    if(passedWord.isNotEmpty()) {
+                        randomWord = passedWord.random()
+                        passedWord.remove(randomWord)
+                    }
+
+                    Log.d("TAG", "PracticeContent: $passedWord")
+
+                }
             )
+
         }
     }
 }
@@ -80,10 +101,38 @@ private fun PracticeBody(
             modifier = Modifier
                 .fillMaxWidth()
                 .height(50.dp),
-            onClick = { /*TODO*/ },
+            onClick = openSimpleDialog,
         ) {
             Text(text = "Next")
         }
     }
 }
+
+
+fun RandomWord(
+    wordList: List<Word>
+): Int {
+    return Random.nextInt(0, wordList.size - 1)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
