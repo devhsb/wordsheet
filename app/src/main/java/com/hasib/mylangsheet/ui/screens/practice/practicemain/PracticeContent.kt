ab7732790742a51482ebf551f49e3764ea4dd399
devhsb <hasibpanjshery2@gmail.com>
2024-04-13 19:31:26 +0100

    practice word count added

--- app/src/main/java/com/hasib/mylangsheet/ui/screens/practice/practicemain/PracticeContent.kt
+++ app/src/main/java/com/hasib/mylangsheet/ui/screens/practice/practicemain/PracticeContent.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,6 +22,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hasib.mylangsheet.data.model.Word
 import com.hasib.mylangsheet.ui.screens.words.wordmain.WordCard
@@ -43,6 +46,10 @@ fun PracticeContent(
         mutableStateOf(passedWord.random())
     }
 
+    var wordPosition by remember {
+        mutableIntStateOf(1)
+    }
+
 
     Scaffold(
         topBar = {
@@ -59,12 +66,17 @@ fun PracticeContent(
 
             PracticeBody(
                 word = randomWord,
+                wordCount = "$wordPosition / ${words.size}",
 
                 openSimpleDialog = {
-
                     if(passedWord.isNotEmpty()) {
-                        randomWord = passedWord.random()
+
                         passedWord.remove(randomWord)
+
+                        if(passedWord.isNotEmpty()) {
+                            wordPosition++
+                            randomWord = passedWord.random()
+                        }
                     }
 
                 }
@@ -74,33 +86,53 @@ fun PracticeContent(
     }
 }
 
+@Preview
+@Composable
+fun PracticeBodyPreview() {
+    PracticeBody(word = Word(0, "Test", "test")) {
+
+    }
+}
 @Composable
 private fun PracticeBody(
     word: Word,
+    wordCount: String = "0/0",
     openSimpleDialog: () -> Unit
 ) {
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(10.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
-        WordCard(
-            word = word,
-            openSimpleDialog = {}
+        Text(
+            text = wordCount,
+            color = MaterialTheme.colorScheme.onPrimaryContainer,
+            style = MaterialTheme.typography.titleSmall
         )
 
-        Spacer(modifier = Modifier.height(70.dp))
-
-        Button(
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .height(50.dp),
-            onClick = openSimpleDialog,
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Next")
+            WordCard(
+                word = word,
+                openSimpleDialog = {}
+            )
+
+            Spacer(modifier = Modifier.height(70.dp))
+
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.CenterHorizontally)
+                    .height(50.dp),
+                onClick = openSimpleDialog,
+            ) {
+                Text(text = "Next")
+            }
         }
     }
 }
