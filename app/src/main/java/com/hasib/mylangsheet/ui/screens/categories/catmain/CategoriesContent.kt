8cf0beb4ff3fe6b0ded94c960a3131b96b88307b
devhsb <hasibpanjshery2@gmail.com>
2024-04-20 15:01:27 +0100

    top and bottombar added

--- app/src/main/java/com/hasib/mylangsheet/ui/screens/categories/catmain/CategoriesContent.kt
+++ app/src/main/java/com/hasib/mylangsheet/ui/screens/categories/catmain/CategoriesContent.kt
@@ -1,8 +1,74 @@
 package com.hasib.mylangsheet.ui.screens.categories.catmain
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.hasib.mylangsheet.ui.screens.words.wordmain.Action
+import com.hasib.mylangsheet.ui.screens.words.wordmain.WordViewModel
+import com.hasib.mylangsheet.ui.shared_components.BottomBar
+import com.hasib.mylangsheet.ui.shared_components.CenterAlignedTopAppbar
 
 @Composable
-fun CategoryContent() {
+fun CategoryContent(
+    wordViewModel: WordViewModel,
+    onWordItemClicked: () -> Unit = {},
+    onPracticeItemClicked: () -> Unit = {},
+    onCategoryItemClicked: () -> Unit = {},
+) {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppbar(
+                title = "Categories",
+                onWordItemClicked = onWordItemClicked,
+                onPracticeItemClicked = onPracticeItemClicked,
+                onCategoryItemClicked = onCategoryItemClicked,
+                wordViewModel = wordViewModel
+            )
+        },
 
+        bottomBar = {
+            BottomBar(
+                onAddButtonClicked = {
+
+                }
+            )
+        }
+
+    ) {
+
+        Box(
+            modifier = Modifier.padding(it)
+        ) {
+
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/hasib/mylangsheet/ui/screens/categories/catmain/CategoriesScreen.kt
+++ app/src/main/java/com/hasib/mylangsheet/ui/screens/categories/catmain/CategoriesScreen.kt
@@ -1,8 +1,19 @@
 package com.hasib.mylangsheet.ui.screens.categories.catmain
 
 import androidx.compose.runtime.Composable
+import com.hasib.mylangsheet.ui.screens.words.wordmain.WordViewModel
 
 @Composable
-fun CategoryScreen() {
-
+fun CategoryScreen(
+    wordViewModel: WordViewModel,
+    onWordItemClicked: () -> Unit = {},
+    onPracticeItemClicked: () -> Unit = {},
+    onCategoryClicked: () -> Unit = {},
+) {
+    CategoryContent(
+        onWordItemClicked = onWordItemClicked,
+        onPracticeItemClicked = onPracticeItemClicked,
+        onCategoryItemClicked = onCategoryClicked,
+        wordViewModel = wordViewModel
+    )
 }
