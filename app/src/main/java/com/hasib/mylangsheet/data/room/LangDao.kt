fc5d22ea77a808e2fc3eb9d4e3854e459448ef7b
devhsb <hasibpanjshery2@gmail.com>
2024-04-29 15:48:13 +0100

    db migration

--- app/src/main/java/com/hasib/mylangsheet/data/room/LangDao.kt
+++ app/src/main/java/com/hasib/mylangsheet/data/room/LangDao.kt
@@ -5,13 +5,19 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Transaction
 import androidx.room.Update
-import com.hasib.mylangsheet.data.model.Word
+import com.hasib.mylangsheet.data.room.entites.Category
+import com.hasib.mylangsheet.data.room.entites.Word
+import com.hasib.mylangsheet.data.room.entites.relations.CategoryWithWord
+import com.hasib.mylangsheet.data.room.entites.relations.WordWithCategory
 import kotlinx.coroutines.flow.Flow
 import com.hasib.mylangsheet.util.Constants.WORD_TABLE_NAME
 
 @Dao
 interface LangDao {
+
+    //word operations
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertWord(word: Word)
 
@@ -24,6 +30,23 @@ interface LangDao {
     @Query("SELECT * FROM $WORD_TABLE_NAME")
     fun getAllWords() : Flow<List<Word>>
 
-    @Query("SELECT * FROM $WORD_TABLE_NAME WHERE id = :id")
-    fun getSelectedWord(id: Int) : Flow<Word>
+    @Query("SELECT * FROM $WORD_TABLE_NAME WHERE word_id = :wordId")
+    fun getSelectedWord(wordId: Int) : Flow<Word>
+
+
+    //category operations
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertCategory(category: Category)
+
+    @Query("SELECT * FROM category")
+    fun getCategories()  : Flow<List<Category>>
+
+    @Transaction
+    @Query("SELECT * FROM Category WHERE category_name = :categoryName")
+    fun getCategoriesWithWords(categoryName: String) : Flow<List<CategoryWithWord>>
+
+    @Transaction
+    @Query("SELECT * FROM $WORD_TABLE_NAME WHERE word = :word")
+    fun getWordsWithCategories(word: String) : Flow<List<WordWithCategory>>
+
 }
