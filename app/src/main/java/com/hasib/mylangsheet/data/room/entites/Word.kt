a8f1a8502efe1cae2a5bd73fd120101d3674947c
devhsb <hasibpanjshery2@gmail.com>
2024-04-30 20:33:58 +0100

    entities updated

--- app/src/main/java/com/hasib/mylangsheet/data/room/entites/Word.kt
+++ app/src/main/java/com/hasib/mylangsheet/data/room/entites/Word.kt
@@ -7,8 +7,9 @@ import com.hasib.mylangsheet.util.Constants.WORD_TABLE_NAME
 
 @Entity(tableName = WORD_TABLE_NAME)
 data class Word(
-    @PrimaryKey(autoGenerate = true) @ColumnInfo("word_id") val id: Int = 0,
+//    @PrimaryKey(autoGenerate = true) @ColumnInfo("word_id") val id: Int = 0,
 
+    @PrimaryKey(autoGenerate = false)
     val word: String,
 
     @ColumnInfo("word_meaning") val wordMeaning: String
