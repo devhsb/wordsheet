f1e49eca863217ccf5ad80ee81eab567889a3e57
devhsb <hasibpanjshery2@gmail.com>
2024-04-30 12:05:45 +0100

    category section improved

---
+++ app/src/main/java/com/hasib/mylangsheet/ui/screens/categories/catmain/CategoryViewModel.kt
@@ -0,0 +1,37 @@
+package com.hasib.mylangsheet.ui.screens.categories.catmain
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.hasib.mylangsheet.data.Repository.LangRepository
+import com.hasib.mylangsheet.data.room.entites.Category
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CategoryViewModel @Inject constructor(
+    private val repository: LangRepository
+) : ViewModel() {
+    init {
+        getCategories()
+    }
+
+    private val _categoryList = MutableStateFlow<List<Category>>(emptyList())
+    val categoryList: StateFlow<List<Category>>
+        get() = _categoryList
+
+
+    private fun getCategories() {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.getCategories.collectLatest {
+                _categoryList.value = it
+            }
+        }
+    }
+
+
+}
