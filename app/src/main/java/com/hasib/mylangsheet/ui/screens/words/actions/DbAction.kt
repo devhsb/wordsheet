af6f4dbd3903805f4871a749c9e80f5c73f7c7e7
devhsb <hasibpanjshery2@gmail.com>
2024-05-03 20:18:21 +0100

    renamed

---
+++ app/src/main/java/com/hasib/mylangsheet/ui/screens/words/actions/DbAction.kt
@@ -0,0 +1,9 @@
+package com.hasib.mylangsheet.ui.screens.words.actions
+
+enum class DbAction {
+    INSERT,
+    UPDATE,
+    MANUAL_UPDATE,
+    DELETE,
+    NO_ACTION
+}
--- app/src/main/java/com/hasib/mylangsheet/ui/screens/words/wordmain/Action.kt
+++
@@ -1,9 +0,0 @@
-package com.hasib.mylangsheet.ui.screens.words.wordmain
-
-enum class Action {
-    INSERT,
-    UPDATE,
-    MANUAL_UPDATE,
-    DELETE,
-    NO_ACTION
-}
