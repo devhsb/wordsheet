810e9408748c0f89229fd763ec81252ea1a55777
devhsb <hasibpanjshery2@gmail.com>
2024-05-06 11:55:31 +0100

    search bug fix

--- app/src/main/java/com/hasib/mylangsheet/ui/shared_components/SearchTextField.kt
+++ app/src/main/java/com/hasib/mylangsheet/ui/shared_components/SearchTextField.kt
@@ -1,12 +1,15 @@
 package com.hasib.mylangsheet.ui.shared_components
 
+import android.os.Build
 import androidx.activity.compose.BackHandler
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -15,6 +18,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import com.hasib.mylangsheet.ui.screens.words.wordmain.WordViewModel
 
 //@Preview(showSystemUi = true)
@@ -23,6 +27,7 @@ import com.hasib.mylangsheet.ui.screens.words.wordmain.WordViewModel
 //    SearchTextField(viewModel())
 //}
 
+@RequiresApi(Build.VERSION_CODES.N)
 @Composable
 fun SearchTextField(
     modifier: Modifier = Modifier,
@@ -34,10 +39,15 @@ fun SearchTextField(
         modifier = modifier
     ) {
 
-        BackHandler(onBack = { wordViewModel.updateWordState(isSearchActive = false) })
+        BackHandler(onBack = {
+            wordViewModel.resetWordState()
+            wordViewModel.getAllWords()
+        })
 
         TextField(
             modifier = Modifier.fillMaxWidth(),
+            singleLine = true,
+            textStyle = TextStyle(fontSize = MaterialTheme.typography.titleMedium.fontSize),
             value = wordUiState.searchQuery,
             onValueChange = {
                 wordViewModel.updateWordState(searchQuery = it)
@@ -49,7 +59,10 @@ fun SearchTextField(
             ),
 
             leadingIcon = {
-                IconButton(onClick = { wordViewModel.updateWordState(isSearchActive = false) }) {
+                IconButton(onClick = {
+                    wordViewModel.resetWordState()
+                    wordViewModel.getAllWords()
+                }) {
                     Icon(
                         imageVector = Icons.Default.ArrowBack,
                         contentDescription = "Back"
