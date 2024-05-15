d6256a483853aebf8254fcb82929c06a7878fb20
devhsb <hasibpanjshery2@gmail.com>
2024-05-15 18:19:14 +0100

    practice hint added

--- app/src/main/java/com/hasib/mylangsheet/ui/screens/practice/practicemain/PracticeContent.kt
+++ app/src/main/java/com/hasib/mylangsheet/ui/screens/practice/practicemain/PracticeContent.kt
@@ -2,6 +2,12 @@ package com.hasib.mylangsheet.ui.screens.practice.practicemain
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,26 +15,39 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import com.hasib.mylangsheet.data.room.entites.word.Word
+import com.hasib.mylangsheet.ui.screens.words.dialog.DialogViewModel
 import com.hasib.mylangsheet.ui.screens.words.wordmain.WordCard
 import com.hasib.mylangsheet.ui.screens.words.wordmain.WordViewModel
 import com.hasib.mylangsheet.ui.shared_components.CenterAlignedTopAppbar
+import kotlinx.coroutines.delay
+import kotlin.math.roundToInt
 
 @RequiresApi(Build.VERSION_CODES.N)
 @Composable
@@ -70,11 +89,9 @@ fun PracticeContent(
                 wordCount = "$wordPosition / ${words.size}",
 
                 openSimpleDialog = {
-                    if(passedWord.isNotEmpty()) {
-
+                    if (passedWord.isNotEmpty()) {
                         passedWord.remove(randomWord)
-
-                        if(passedWord.isNotEmpty()) {
+                        if (passedWord.isNotEmpty()) {
                             wordPosition++
                             randomWord = passedWord.random()
                         }
@@ -90,21 +107,43 @@ fun PracticeContent(
 @Preview
 @Composable
 fun PracticeBodyPreview() {
-    PracticeBody(word = Word( "Test", "test")) {
+    PracticeBody(word = Word("Test", "test")) {
 
     }
 }
+
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 private fun PracticeBody(
     word: Word,
     wordCount: String = "0/0",
     openSimpleDialog: () -> Unit
 ) {
+    val haptics = LocalHapticFeedback.current
+    val dialogViewModel = DialogViewModel()
+    val context = LocalContext.current
+
+    var isDialogOpened by remember {
+        mutableStateOf(false)
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(10.dp),
     ) {
+
+        if (isDialogOpened) {
+            MeaningDialog(
+                onDismiss = { isDialogOpened = false },
+                word = word
+            )
+
+            LaunchedEffect(Unit) {
+                delay(1000)
+                isDialogOpened = false
+            }
+        }
 
         Text(
             text = wordCount,
@@ -119,9 +158,13 @@ private fun PracticeBody(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             WordCard(
-                modifier = Modifier.height(70.dp),
+                modifier = Modifier
+                    .height(70.dp),
                 word = word,
-                openSimpleDialog = {}
+                onLongPress = {
+                    haptics.performHapticFeedback(HapticFeedbackType.LongPress)
+                    isDialogOpened = true
+                }
             )
 
             Spacer(modifier = Modifier.height(70.dp))
@@ -139,13 +182,37 @@ private fun PracticeBody(
     }
 }
 
+@Composable
+private fun MeaningDialog(
+    onDismiss: () -> Unit = {},
+    word: Word
+) {
+    Dialog(onDismissRequest = onDismiss) {
+        Card(
+            modifier = Modifier
+                .padding(10.dp)
+                .background(
+                    MaterialTheme.colorScheme.surfaceVariant,
+                    shape = MaterialTheme.shapes.large
+                )
+                .padding(10.dp)
+        ) {
 
+            Text(
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                text = word.wordMeaning,
+                style = MaterialTheme.typography.titleLarge,
+                color = MaterialTheme.colorScheme.onSecondaryContainer
+            )
+        }
+    }
+}
 
-
-
-
-
-
+@Preview
+@Composable
+private fun MeaningDialogPreview() {
+    MeaningDialog(word = Word(word = "123", wordMeaning = "meaningmeaningmeaningmeaningmeaning"))
+}
 
 
 
