2377cfd5b97f49231fd54d521e153d8f084a1aeb
devhsb <hasibpanjshery2@gmail.com>
2024-05-15 14:06:05 +0100

    practice for specific category added

--- app/src/main/java/com/hasib/mylangsheet/ui/screens/practice/practicemain/PracticeScreen.kt
+++ app/src/main/java/com/hasib/mylangsheet/ui/screens/practice/practicemain/PracticeScreen.kt
@@ -1,16 +1,22 @@
 package com.hasib.mylangsheet.ui.screens.practice.practicemain
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
+import com.hasib.mylangsheet.data.room.entites.word.Word
 import com.hasib.mylangsheet.ui.screens.words.wordmain.WordViewModel
 
+@RequiresApi(Build.VERSION_CODES.N)
 @Composable
 fun PracticeScreen(
+    words: List<Word>,
     wordViewModel: WordViewModel,
     onWordItemClicked: () -> Unit,
     onPracticeItemClicked: () -> Unit,
     onCategoryItemClicked: () -> Unit
 ) {
     PracticeContent(
+        words = words,
         wordViewModel = wordViewModel,
         onWordItemClicked = onWordItemClicked,
         onPracticeItemClicked = onPracticeItemClicked,
