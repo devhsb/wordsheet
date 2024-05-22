020fe5993b124fe7244adf580811f7f0e729f293
devhsb <hasibpanjshery2@gmail.com>
2024-05-22 15:20:16 +0100

    backup & restore menu added

--- app/src/main/java/com/hasib/mylangsheet/ui/shared_components/TopAppbar.kt
+++ app/src/main/java/com/hasib/mylangsheet/ui/shared_components/TopAppbar.kt
@@ -1,20 +1,31 @@
 package com.hasib.mylangsheet.ui.shared_components
 
+import android.content.Context
+import android.content.Intent
 import android.os.Build
+import android.os.Environment
+import android.provider.Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
@@ -24,22 +35,33 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.MultiplePermissionsState
+import com.google.accompanist.permissions.PermissionState
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.accompanist.permissions.shouldShowRationale
+import com.hasib.mylangsheet.data.room.backup.BackupDatabase.backupDb
+import com.hasib.mylangsheet.data.room.backup.RestoreDatabase.restoreDb
 import com.hasib.mylangsheet.ui.screens.words.wordmain.WordViewModel
-import com.hasib.mylangsheet.ui.theme.MyLangsheetTheme
+import java.util.jar.Manifest
 
 @RequiresApi(Build.VERSION_CODES.N)
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalPermissionsApi::class)
 @Composable
 fun CenterAlignedTopAppbar(
     title: String,
@@ -48,16 +70,20 @@ fun CenterAlignedTopAppbar(
     onCategoryItemClicked: () -> Unit,
     wordViewModel: WordViewModel
 ) {
+    val context = LocalContext.current
+
     val wordUiState by wordViewModel.wordUiState.collectAsState()
 
     Column(
         modifier = Modifier
-            .fillMaxWidth()
+            .fillMaxWidth(),
     ) {
-        var expanded by wordViewModel.topbarDropDownState
+        var menuExpanded by wordViewModel.topbarDropDownState
+
+        var backupRestoreMenuExtended by remember { mutableStateOf(false) }
 
         CenterAlignedTopAppBar(title = {
-            if(wordUiState.isSearchActive) {
+            if (wordUiState.isSearchActive) {
                 SearchTextField(
                     wordViewModel = wordViewModel
                 )
@@ -66,7 +92,7 @@ fun CenterAlignedTopAppbar(
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(horizontal = 10.dp)
-                        .clickable { expanded = !expanded },
+                        .clickable { menuExpanded = !menuExpanded },
                     verticalAlignment = Alignment.CenterVertically,
                 ) {
 
@@ -97,36 +123,96 @@ fun CenterAlignedTopAppbar(
                             contentDescription = null,
                         )
                     }
+
+                    IconButton(onClick = {
+                        backupRestoreMenuExtended = !backupRestoreMenuExtended
+                    }) {
+                        Icon(
+                            imageVector = Icons.Filled.MoreVert,
+                            contentDescription = null
+                        )
+                    }
+
+
                 }
             }
         })
 
-
         DropdownMenu(
             modifier = Modifier
                 .fillMaxWidth(),
-            expanded = expanded,
-            onDismissRequest = { expanded = !expanded }) {
-
-            DropdownMenuItem(
+            expanded = menuExpanded,
+            onDismissRequest = { menuExpanded = !menuExpanded }
+        ) {
+            AppbarDropdownMenuItem(
                 text = "Words",
                 onItemClicked = onWordItemClicked
             )
-            DropdownMenuItem(
+            AppbarDropdownMenuItem(
                 text = "Practice",
                 onItemClicked = onPracticeItemClicked
             )
-            DropdownMenuItem(
+            AppbarDropdownMenuItem(
                 text = "Categories",
                 onItemClicked = onCategoryItemClicked
             )
         }
 
+        Box(
+            modifier = Modifier
+                .padding(horizontal = 20.dp)
+                .align(Alignment.End),
+        ) {
+            DropdownMenu(
+                expanded = backupRestoreMenuExtended,
+                onDismissRequest = { backupRestoreMenuExtended = !backupRestoreMenuExtended }
+            ) {
+
+                BackupRestoreDropdownItem(
+                    text = "Backup",
+                    onItemClicked = {
+                        if(!Environment.isExternalStorageManager()) {
+                            Toast.makeText(context, "Please grant permission", Toast.LENGTH_SHORT).show()
+                            val openSettings = Intent(ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION)
+                            context.startActivity(openSettings)
+                        }else{
+                            backupDb(context = context)
+                        }
+
+                        backupRestoreMenuExtended = !backupRestoreMenuExtended
+                    }
+                )
+
+                BackupRestoreDropdownItem(
+                    text = "Restore",
+                    onItemClicked = {
+
+                        if(!Environment.isExternalStorageManager()) {
+                            Toast.makeText(context, "Please grant permission", Toast.LENGTH_SHORT).show()
+                            val openSettings = Intent(ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION)
+                            context.startActivity(openSettings)
+                        }else{
+                            restoreDb(context = context)
+                            restartApp(context = context)
+                        }
+
+                        backupRestoreMenuExtended = !backupRestoreMenuExtended
+                    }
+                )
+            }
+        }
     }
 }
 
+fun restartApp(context: Context) {
+    val intent = context.packageManager.getLaunchIntentForPackage(context.packageName)
+    intent?.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+    context.startActivity(intent)
+    Runtime.getRuntime().exit(0) // Optional: Use this line to forcefully close the current process
+}
+
 @Composable
-fun DropdownMenuItem(
+private fun AppbarDropdownMenuItem(
     text: String,
     onItemClicked: () -> Unit
 ) {
@@ -149,16 +235,16 @@ fun DropdownMenuItem(
     )
 }
 
-//@Preview
-//@Composable
-//fun CenterAlignedTopAppbarPreview() {
-//    CenterAlignedTopAppbar(title = "Words", {}, {}, {}, viewModel())
-//}
-//
-//@Preview
-//@Composable
-//fun CenterAlignedTopAppbarPreviewDark() {
-//    MyLangsheetTheme(useDarkTheme = true) {
-//        CenterAlignedTopAppbar(title = "Words", {}, {}, {}, viewModel())
-//    }
-//}
+
+@Composable
+private fun BackupRestoreDropdownItem(
+    text: String,
+    onItemClicked: () -> Unit
+) {
+    DropdownMenuItem(
+        text = {
+            Text(text = text)
+        },
+        onClick = onItemClicked
+    )
+}
